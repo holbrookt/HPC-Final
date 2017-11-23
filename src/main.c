1712cd35471389dfacd552c4d1f9ec8b8b5a2000
Matan Kaminski <matan.kaminski@gmail.com>
2017-11-23 12:46:59 -0500

    Saving before weights refactor

--- src/main.c
+++ src/main.c
@@ -4,8 +4,7 @@
 #include <string.h>
 
 //heat_scale dataset is 270x13
-double data[270][13];
-double solutions[270];
+//double solutions[270];
 double weights[13] = { 0 };
 
 
@@ -14,13 +13,13 @@ double sigmoid(double x) {
     return 1.0 / (1.0 + pow(e, -x));
 }
 
-void readfile(char* filename) {
+void readfile(char* filename, double ** data, double *solutions, int number_of_features) {
     FILE* fp = fopen(filename, "r");
     //char*
     char line[150];
     int example_number = 0;
     while(fgets(line, sizeof(line), fp)){
-        char ** line_tokens = malloc(14*sizeof(char*));
+        char ** line_tokens = malloc((number_of_features+1)*sizeof(char*));
         int i = 0;
         int term_length = strcspn(line, "\n\r");
         line[term_length] = '\0';
@@ -31,7 +30,7 @@ void readfile(char* filename) {
             line_tokens[++i] = strtok(NULL, " ");
         }
         // bias
-        data[example_number][0] = 1;
+        // data[example_number][0] = 1;
         
         // Align the data. Start at 1 and continue until null because
         // we don't know the length of the line_tokens array because
@@ -46,7 +45,7 @@ void readfile(char* filename) {
         }
         
         printf("Data %d:   ", example_number); 
-        for (i = 0; i<=13; i++) {
+        for (i = 1; i<=number_of_features; i++) {
             printf("%.3f  ", data[example_number][i]);
         }
         printf("\n");
@@ -67,13 +66,22 @@ double normalize(double * left, double * right) {
     return sqrt(sum);
 }
 
-void logisticRegression() {
+double predict( double * row, double * weights) {
+    double sol = weights[0];
+    int i;
+    for ( i = 1; i < 13; i++) {
+       sol += weights[i] * row[i-1];
+    }
+    return sigmoid(sol);
+}
+
+void logisticRegression(double **data, double *solutions) {
 
     // t
     double epsilon = 0.000001;
     // the learning rate
-    double gamma = 0.00005;
-    int max_iters = 20000;
+    double gamma = 0.0001;
+    int max_iters = 10000;
     int iter = 0;
     
     printf("Starting logistic Regression\n");
@@ -81,16 +89,19 @@ void logisticRegression() {
     double prev_weights[13] = { 0 };
     int i, j, k;
     while (iter < max_iters) {
-        for (i=0; i < 13; i++) {
-            double gradient = 0;
-            for (j=0; j < 270; j++) {
-                double z_j = prev_weights[0]; //data[j][0];
-                for (k=1; k < 13; k++) {
-                    z_j += prev_weights[k] * data[j][k];
-                }
-                gradient = solutions[j] * data[j][i] * sigmoid(-solutions[j]*z_j);
+        double gradient = 0;
+        double error;
+        for (j=0; j < 270; j++) {
+            double sol = predict(data[j], prev_weights);
+            error = solutions[j] - sol;
+            weights[0] = prev_weights[0] + gamma * error * sol * (1- sol);
+            //gradient = solutions[j] * data[j][i] * sol;
+            if (j == 268) {
+                printf("[%d] Error: %f Weight diff: %f\n", j, error, weights[0] - prev_weights[0]);
             }
-            weights[i] = prev_weights[i] + gamma * gradient;
+            for (i=1; i < 13; i++) {
+                weights[i] = prev_weights[i] + gamma * error * sol * (1 - sol) * data[j][i];
+            }
         }
         double dist = normalize(weights, prev_weights);
         if ( dist < epsilon) {
@@ -108,24 +119,42 @@ void logisticRegression() {
     printf("\n");
 }
 
-int test() {
+int test(double ** data, double *solutions) {
     int i, j;
+    int total = 0;
+    int correct = 0;
     for (i = 0; i < 270; i++) {
-        double sol = weights[0];
-        for (j = 1; j < 13; j++) {
-            sol += data[i][j]*weights[j];
+        double solution = predict(data[i], weights);
+        if (i == 268) {
+            printf("Real %f -- Predicted %f\n", solutions[i], solution);
         }
-        printf("Real %f -- Predicted %f\n", solutions[i], sigmoid(sol));
+        if (round(solution) == solutions[i]) {
+            correct++;
+        }
+        total++;
     }
+    printf("Percent Error = %d/%d\n", correct, total);
     return 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Usage: ./LogisticRegression DataFile #_of_entries #_of_features\n");
+        exit(1);
+    }
+    double number_of_entries = atoi(argv[2]);
+    double number_of_features = atoi(argv[3]);
+    double **data = malloc( number_of_entries * sizeof(double *));
+    double * solutions = malloc( number_of_entries * sizeof(double));
+    int i =0;
+    for (i = 0; i < number_of_entries; i++) {
+        data[i] = malloc(number_of_features * sizeof(double));
+    }
     printf("Reading File\n");
-    readfile("data/heart_scale"); 
+    readfile(argv[1], data, solutions, number_of_features); 
     printf("Running Logistic Regresion\n");
-    logisticRegression();
-    test();
+    logisticRegression(data, solutions);
+    test(data, solutions);
     printf("Done\n");
     return 0;
 }
