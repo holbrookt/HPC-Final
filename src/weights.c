486e447c7d0886913c87f197dedf5c2223c81df4
Matan Kaminski <kaminski.ma@discovery2.neu.edu>
2017-11-27 17:25:35 -0500

    Don't want to lose progress

--- src/weights.c
+++ src/weights.c
@@ -8,3 +8,13 @@ void print_weights(Weights_t weights) {
     }
     printf("\n");
 }
+
+void print_weights_ranked(Weights_t weights, int rank) {
+    int i;
+    printf("[%d] Result: ", rank);
+    for (i = 0; i < weights->length; i++) {
+        printf("%f ", weights->values[i]);
+    }
+    printf("\n");
+}
+
