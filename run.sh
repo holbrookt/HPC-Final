f3fb068042318cdbf7f319005a3f4a1398ee37a9
Matan Kaminski <kaminski.ma@discovery2.neu.edu>
2017-11-28 02:14:01 -0500

    Adding run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for j in 1 10 100; do
+    echo "Running Batch_size ${j}"
+    for i in {1..5}; do
+        mpirun -np 13 ./DownpourLogisticRegression data/heart_scale 270 13 ${j} | grep "Took"
+    done
+done
