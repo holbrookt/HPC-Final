79d273bdea79a7da9742547ceb70d4aef05c1e5d
holbrookt <holbrook.t@husky.neu.edu>
2017-11-27 19:24:07 -0800

    adding song data

--- data/song_year_format.py
+++ data/song_year_format.py
@@ -4,20 +4,19 @@ infile = open("YearPredictionMSD.txt","r")
 outfile = open("songs_test.txt","w")
 lines = 0
 for line in infile:
-    if lines < 50000:
+    if lines < 30000:
+        lines += 1
         continue
     data = line.rstrip().split(",")
     if int(data[0]) >= 2000:
         outfile.write("1")
-        print "{} {}".format(data[0], 1)
     else:
         outfile.write("0")
-        print "{} {}".format(data[0], 0)
     for i in range(1, len(data)):
         outfile.write(" {}:{}".format(i, data[i]))
     outfile.write("\n");
     lines += 1
-    if lines == 60000:
+    if lines == 40000:
         break
 
 infile.close()
